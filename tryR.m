c133d74876ea49270373f7a4d71b3f77d6cfa214
Radoslav Gabrovski <rado_lxg@yahoo.com>
2013-02-24 14:16:39 +0000

    NoralizeRGB now working

---
+++ tryR.m
@@ -0,0 +1,18 @@
+function img = tryR(image)
+[m,n,w] = size(image);
+img = double(ones(m,n,w));
+for i = 1:m
+    for j = 2:n
+        if(image(i,j,1) > 100 && image(i,j,2) < 100 && image(i,j,3) < 50)
+            img(i,j,1) = image(i,j,1);
+            img(i,j,2) = image(i,j,2);
+            img(i,j,3) = image(i,j,3);
+        else
+            img(i,j,1) = 0;
+            img(i,j,2) = 0;
+            img(i,j,3) = 0;
+        end
+    end
+end
+img = uint8(img);
+end
